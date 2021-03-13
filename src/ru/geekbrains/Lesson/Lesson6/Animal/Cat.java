221defbda279c1af41c16babaab57c6479adad88
merlin <oymerlinyo@gmail.com>
2021-03-13 12:41:48 +0300

    6 Lesson

--- src/ru/geekbrains/Lesson/Lesson6/Animal/Cat.java
+++ src/ru/geekbrains/Lesson/Lesson6/Animal/Cat.java
@@ -1,8 +1,8 @@
 package ru.geekbrains.Lesson.Lesson6.Animal;
 
 public class Cat extends Animal{
-    public static final int MAX_RUN = 200;
-    public static final int MAX_SWIM = 0;
+    private static final int MAX_RUN = 200;
+    private static final int MAX_SWIM = 0;
     private static final String ANIMAL = "Cat";
 
     public Cat(String name, String color, int age){
--- src/ru/geekbrains/Lesson/Lesson6/Animal/Dog.java
+++ src/ru/geekbrains/Lesson/Lesson6/Animal/Dog.java
@@ -1,8 +1,8 @@
 package ru.geekbrains.Lesson.Lesson6.Animal;
 
 public class Dog extends Animal{
-    public static final int MAX_RUN = 500;
-    public static final int MAX_SWIM = 10;
+    private static final int MAX_RUN = 500;
+    private static final int MAX_SWIM = 10;
     private static final String ANIMAL = "Dog";
 
     public Dog(String name, String color, int age){
